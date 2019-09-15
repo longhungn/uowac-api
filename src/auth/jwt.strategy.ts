06937332568fc054f68e2db8281aca1d885d939e
Long Hung Nguyen <longhungn98@gmail.com>
2019-09-16 00:44:37 +1000

    implement passport jwt and scopesguard

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import {
+  Strategy as PassportJwtStrategy,
+  StrategyOptions as JwtStrategyOptions,
+  ExtractJwt,
+} from 'passport-jwt';
+import { passportJwtSecret } from 'jwks-rsa';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(PassportJwtStrategy) {
+  constructor() {
+    const opts: JwtStrategyOptions = {
+      algorithms: ['RS256'],
+      issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+      audience: process.env.AUTH0_AUDIENCE,
+      secretOrKeyProvider: passportJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+      }),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    };
+    super(opts);
+  }
+
+  //return the request.user object
+  async validate(payload: any) {
+    console.log(payload);
+
+    return {
+      userId: payload.sub,
+      username: payload.username,
+      scope: payload.scope,
+    };
+  }
+}
