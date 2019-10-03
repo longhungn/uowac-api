e2e2589e0d30aa2a34942de3f9050fe68186ee9f
Long Hung Nguyen <longhungn98@gmail.com>
2019-10-03 19:44:37 +1000

    add scope guard

--- src/social/controller/stats.controller.ts
+++ src/social/controller/stats.controller.ts
@@ -7,59 +7,64 @@ import {
 } from '@nestjs/common';
 import { StatsService } from '../service/stats.service';
 import { AuthGuard } from '@nestjs/passport';
+import { ScopesGuard } from '../../auth/scopes.guard';
+import { Scopes } from '../../auth/scopes.decorator';
 
 @Controller('stats')
 export class StatsController {
   constructor(private readonly statsService: StatsService) {}
 
-  // @Get()
-  // async getTest(): Promise<{}> {
-  //   return await this.statsService.getTest();
-  // }
   @Get('/visits')
-  @UseGuards(AuthGuard())
+  @UseGuards(AuthGuard(), ScopesGuard)
+  @Scopes('view:analytics')
   async getVisitsWithinDateRange(@Query() query): Promise<{}> {
     const { fromDate, toDate } = query;
     return await this.statsService.getVisitsWithinDateRange(fromDate, toDate);
   }
 
   @Get('/comments')
-  @UseGuards(AuthGuard())
+  @UseGuards(AuthGuard(), ScopesGuard)
+  @Scopes('view:analytics')
   async getCommentsWithinDateRange(@Query() query): Promise<{}> {
     const { fromDate, toDate } = query;
     return await this.statsService.getCommentsWithinDateRange(fromDate, toDate);
   }
 
   @Get('/likes')
-  @UseGuards(AuthGuard())
+  @UseGuards(AuthGuard(), ScopesGuard)
+  @Scopes('view:analytics')
   async getLikesWithinDateRange(@Query() query): Promise<{}> {
     const { fromDate, toDate } = query;
     return await this.statsService.getLikesWithinDateRange(fromDate, toDate);
   }
 
   @Get('/total/visits')
-  @UseGuards(AuthGuard())
+  @UseGuards(AuthGuard(), ScopesGuard)
+  @Scopes('view:analytics')
   async getTotalVisits(): Promise<{}> {
     const totalVisits = await this.statsService.getTotalVisits();
     return { totalVisits };
   }
 
   @Get('/total/likes')
-  @UseGuards(AuthGuard())
+  @UseGuards(AuthGuard(), ScopesGuard)
+  @Scopes('view:analytics')
   async getTotalLikes(): Promise<{}> {
     const totalLikes = await this.statsService.getTotalLikes();
     return { totalLikes };
   }
 
   @Get('/total/comments')
-  @UseGuards(AuthGuard())
+  @UseGuards(AuthGuard(), ScopesGuard)
+  @Scopes('view:analytics')
   async getTotalComments(): Promise<{}> {
     const totalComments = await this.statsService.getTotalComments();
     return { totalComments };
   }
 
   @Get('/total/users')
-  @UseGuards(AuthGuard())
+  @UseGuards(AuthGuard(), ScopesGuard)
+  @Scopes('view:analytics')
   async getTotalUsers(): Promise<{}> {
     const totalUsers = await this.statsService.getTotalUsers();
     return { totalUsers };
