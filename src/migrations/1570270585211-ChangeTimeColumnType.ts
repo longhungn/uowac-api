a2a4fd50d69a618ee13599f2d800851b939f15a3
Long Hung Nguyen <longhungn98@gmail.com>
2019-10-05 21:53:45 +1000

    fix migration

--- src/migrations/1570270585211-ChangeTimeColumnType.ts
+++ src/migrations/1570270585211-ChangeTimeColumnType.ts
@@ -3,33 +3,21 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class ChangeTimeColumnType1570270585211 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`ALTER TABLE "visit" DROP COLUMN "visitTime"`, undefined);
-        await queryRunner.query(`ALTER TABLE "visit" ADD "visitTime" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`, undefined);
-        await queryRunner.query(`ALTER TABLE "like" DROP COLUMN "likedTime"`, undefined);
-        await queryRunner.query(`ALTER TABLE "like" ADD "likedTime" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`, undefined);
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "joinDate"`, undefined);
-        await queryRunner.query(`ALTER TABLE "user" ADD "joinDate" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`, undefined);
-        await queryRunner.query(`ALTER TABLE "comment" DROP COLUMN "createdTime"`, undefined);
-        await queryRunner.query(`ALTER TABLE "comment" ADD "createdTime" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`, undefined);
-        await queryRunner.query(`ALTER TABLE "comment" DROP COLUMN "updatedTime"`, undefined);
-        await queryRunner.query(`ALTER TABLE "comment" ADD "updatedTime" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`, undefined);
-        await queryRunner.query(`ALTER TABLE "sculpture_image" DROP COLUMN "created"`, undefined);
-        await queryRunner.query(`ALTER TABLE "sculpture_image" ADD "created" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()`, undefined);
+        await queryRunner.query(`ALTER TABLE "visit" ALTER COLUMN "visitTime" TYPE TIMESTAMP WITH TIME ZONE USING "visitTime" AT TIME ZONE 'UTC'`, undefined);
+        await queryRunner.query(`ALTER TABLE "like" ALTER COLUMN "likedTime" TYPE TIMESTAMP WITH TIME ZONE USING "likedTime" AT TIME ZONE 'UTC'`, undefined);
+        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "joinDate" TYPE TIMESTAMP WITH TIME ZONE USING "joinDate" AT TIME ZONE 'UTC'`, undefined);
+        await queryRunner.query(`ALTER TABLE "comment" ALTER COLUMN "createdTime" TYPE TIMESTAMP WITH TIME ZONE USING "createdTime" AT TIME ZONE 'UTC'`, undefined);
+        await queryRunner.query(`ALTER TABLE "comment" ALTER COLUMN "updatedTime" TYPE TIMESTAMP WITH TIME ZONE USING "updatedTime" AT TIME ZONE 'UTC'`, undefined);
+        await queryRunner.query(`ALTER TABLE "sculpture_image" ALTER COLUMN "created" TYPE TIMESTAMP WITH TIME ZONE USING "created" AT TIME ZONE 'UTC'`, undefined);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`ALTER TABLE "sculpture_image" DROP COLUMN "created"`, undefined);
-        await queryRunner.query(`ALTER TABLE "sculpture_image" ADD "created" TIMESTAMP NOT NULL DEFAULT now()`, undefined);
-        await queryRunner.query(`ALTER TABLE "comment" DROP COLUMN "updatedTime"`, undefined);
-        await queryRunner.query(`ALTER TABLE "comment" ADD "updatedTime" TIMESTAMP NOT NULL DEFAULT now()`, undefined);
-        await queryRunner.query(`ALTER TABLE "comment" DROP COLUMN "createdTime"`, undefined);
-        await queryRunner.query(`ALTER TABLE "comment" ADD "createdTime" TIMESTAMP NOT NULL DEFAULT now()`, undefined);
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "joinDate"`, undefined);
-        await queryRunner.query(`ALTER TABLE "user" ADD "joinDate" TIMESTAMP NOT NULL DEFAULT now()`, undefined);
-        await queryRunner.query(`ALTER TABLE "like" DROP COLUMN "likedTime"`, undefined);
-        await queryRunner.query(`ALTER TABLE "like" ADD "likedTime" TIMESTAMP NOT NULL DEFAULT now()`, undefined);
-        await queryRunner.query(`ALTER TABLE "visit" DROP COLUMN "visitTime"`, undefined);
-        await queryRunner.query(`ALTER TABLE "visit" ADD "visitTime" TIMESTAMP NOT NULL DEFAULT now()`, undefined);
+        await queryRunner.query(`ALTER TABLE "sculpture_image" ALTER COLUMN "created" TYPE TIMESTAMP USING "created" AT TIME ZONE 'UTC'`, undefined);
+        await queryRunner.query(`ALTER TABLE "comment" ALTER COLUMN "updatedTime" TYPE TIMESTAMP USING "updatedTime" AT TIME ZONE 'UTC'`, undefined);
+        await queryRunner.query(`ALTER TABLE "comment" ALTER COLUMN "createdTime" TYPE TIMESTAMP USING "createdTime" AT TIME ZONE 'UTC'`, undefined);
+        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "joinDate" TYPE TIMESTAMP USING "joinDate" AT TIME ZONE 'UTC'`, undefined);
+        await queryRunner.query(`ALTER TABLE "like" ALTER COLUMN "likedTime" TYPE TIMESTAMP USING "likedTime" AT TIME ZONE 'UTC'`, undefined);
+        await queryRunner.query(`ALTER TABLE "visit" ALTER COLUMN "visitTime" TYPE TIMESTAMP USING "visitTime" AT TIME ZONE 'UTC'`, undefined);
     }
 
 }
